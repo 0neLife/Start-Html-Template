467552dc7931c57ce93df9629bf9ae4e3ebfb127
Yuriy Butsan <butsan.yuriy@gmail.com>
2018-04-14 00:56:50 +0300

    start-page-for-website

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+/*Write code here*/
+
+  //E-mail Ajax Send
+  $("add-class").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      alert("Thank you!");
+      setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
+});
